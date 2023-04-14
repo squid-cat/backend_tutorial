e6f59dd623645222d65fe96a819ee24aac82ddda
squid-cat <kaikai120626@gmail.com>
2023-04-14 22:23:59 +0900

    format: フォーマット整備

--- domain/user.ts
+++ domain/user.ts
@@ -12,23 +12,23 @@ export const userSchema = zod.object({
 export type User = zod.infer<typeof userSchema>;
 
 export type UserReadEvent = {
-  targetId: User["id"]
-}
+  targetId: User["id"];
+};
 export type UserCreateEvent = {
-  loginId: User["loginId"]
-  password: User["password"]
-}
+  loginId: User["loginId"];
+  password: User["password"];
+};
 export type UserUpdateEvent = {
-  targetId: User["id"]
-  loginId: User["loginId"]
-  password: User["password"]
-}
-export type UserDeleteEvent  = {
-  targetId: User["id"]
-}
+  targetId: User["id"];
+  loginId: User["loginId"];
+  password: User["password"];
+};
+export type UserDeleteEvent = {
+  targetId: User["id"];
+};
 
-type UserHandler = <Payload>(payload: Payload) => User
-export type UserReadEventHandler = UserHandler
-export type UserCreateEventHandler = (payload: UserCreateEvent) => User
-export type UserUpdateEventHandler = (payload: UserUpdateEvent) => User
-export type UserDeleteEventHandler = UserHandler
+type UserHandler = <Payload>(payload: Payload) => User;
+export type UserReadEventHandler = UserHandler;
+export type UserCreateEventHandler = (payload: UserCreateEvent) => User;
+export type UserUpdateEventHandler = (payload: UserUpdateEvent) => User;
+export type UserDeleteEventHandler = UserHandler;
